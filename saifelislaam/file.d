25734b69773333fabb66483d24c38636c39d1159
saifelislaam <152005593+saifelislaam@users.noreply.github.com>
2023-11-25 13:25:25 +0100

    Create file.d

---
+++ saifelislaam/file.d
@@ -0,0 +1,20 @@
+
+function moveButtonRandomly() { const btnRect =
+
+198
+
+noBtn.getBoundingClientRect(); const maxWidth = window.innerWidth - btnRect.width;
+
+const maxHeight = window.innerHeight - btnRect.height;
+
+const randomX =
+
+Math.floor(Math.random() * maxWidth);
+
+const r}andomY = Math.floor(Math.random() * maxHeight);
+
+noBtn.style.setProperty("left", "${randomX} px');
+
+noBtn.style.setProperty("top", "${randomY}
+
+px');
